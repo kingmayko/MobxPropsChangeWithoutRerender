15c90c85f8197fc4d09cbd80b7afcd105399816a
Jiajiun <Jiajiun_Yang@wistron.com>
2016-10-25 10:27:55 +0800

    test mobx inject

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {TodoView} from './TodoView.material'
 import {TodoViewModel} from './TodoViewModel'
 
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import {observer,inject,Provider} from 'mobx-react';
 
 // Needed for onTouchTap by material-ui
 // http://stackoverflow.com/a/34015469/988941
@@ -15,5 +16,39 @@ injectTapEventPlugin();
 // create a viewModel singleton
 const model = new TodoViewModel()
 
+
+
 // render the editor
-render(<TodoView model={model} />, document.getElementById('root'))
+render(
+	<TodoView model={model} /> 
+, document.getElementById('root'))
+
+
+
+
+// inject test
+// var e = React.createElement;
+//  var C = inject(
+//             function(stores, props, context) {
+//                 return {
+//                     zoom: stores.foo,
+//                     baz: props.baz * 2
+//                 }
+//             }
+//         )(observer(React.createClass({
+//             render: function() {
+//                 return e("div", {}, "context:" + this.props.zoom + this.props.baz);
+//             }
+//         })));
+
+//         var B = React.createClass({
+//             render: function() {
+//                 return e(C, { baz: 42 });
+//             }
+//         });
+
+// var A = React.createClass({
+//     render: function() {
+//         return e(Provider, { foo1: "bar"}, e(B, {}))
+//     }
+// })
